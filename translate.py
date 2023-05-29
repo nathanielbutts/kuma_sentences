1449dbfa4fdbec0b6b0df1d508b83d93bf025659
Nathaniel Butts <67642785+nathanielbutts@users.noreply.github.com>
2023-05-29 10:11:50 -0500

    Improvements made
    Added one_for_one translation function, improved notes on Setup section, added sanity check on save_audio to ensure that script is also creating the audio to prevent crashing.

--- translate.py
+++ translate.py
@@ -7,10 +7,12 @@ import json
 # Setup items
 #############
 
-make_audio = True
-save_audio = True
-audio_path = "audio\\"
-anki_path = "anki\\"
+make_audio = True # True synthesizes speech, False does not and only gives text response
+save_audio = True # True will save the synthesized speech as mp3, false does not
+create_or_translate = "translate" # Set to create to make a target language sentence from a vocab, set to translate to just do a one for one translation
+audio_path = "audio\\" # Path to save the audio files
+anki_path = "anki\\" # Path to save anki files (future improvement)
+target_lang = "Japanese" # Set this as the language you wish to return (i.e. Japanese, Russian, English, etc.)
 
 try:  # Set up your OpenAI API key
     with open('secrets.json') as f:
@@ -30,9 +32,31 @@ rate = 0.85
 # Functions
 ############
 
-def generate_japanese_sentence(input_text):
+def generate_target_sentence(input_text):
     # Set the parameters for the ChatGPT completion
-    prompt = f"Create a beginner-level Japanese sentence using the following vocabulary word:\n{input_text}\nTranslation:"
+    prompt = f"Create a beginner-level {target_lang} sentence using the following vocabulary word:\n{input_text}\nTranslation:"
+    temperature = 0.8
+    max_tokens = 50
+
+    # Generate a response from ChatGPT
+    response = openai.Completion.create(
+        engine='text-davinci-003',
+        prompt=prompt,
+        temperature=temperature,
+        max_tokens=max_tokens,
+        n=1,
+        stop=None,
+        #temperature_decay=0.6
+    )
+
+    # Extract the generated sentence from the response
+    generated_sentence = response.choices[0].text.strip()
+
+    return generated_sentence
+
+def one_to_one_translation(input_text):
+    # Set the parameters for the ChatGPT completion
+    prompt = f"Translate {input_text} into {target_lang}:"
     temperature = 0.8
     max_tokens = 50
 
@@ -58,7 +82,7 @@ def save_output_file(input_word, generated_sentence):
         writer = csv.writer(csv_file)
         writer.writerow([input_word, generated_sentence])
 
-def text_to_speech(text): #Synthesizes the speech into japanese
+def text_to_speech(text): #Synthesizes the speech into the chosen target language
     # Initialize the pyttsx3 engine
     engine = pyttsx3.init()
     voices = engine.getProperty('voices')
@@ -76,22 +100,28 @@ def save_speech_file(text,filename):
     engine.runAndWait()
 
 def main():
-    # Read the Japanese text from the CSV file
+    # Read the input text from the CSV file
     with open('input.csv', 'r', encoding='utf-8') as csv_file:
         reader = csv.reader(csv_file)
         for row in reader:
-            japanese_text = row[0]
+            input_text = row[0]
             # Generate a beginner-level sentence
-            beginner_sentence = generate_japanese_sentence(japanese_text)
-            print(f"Input: {japanese_text}")
-            print(f"Generated Beginner-level Sentence: {beginner_sentence}")
-            if make_audio:
+            if create_or_translate == "create":
+                beginner_sentence = generate_target_sentence(input_text)
+                print(f"Input: {input_text}")
+                print(f"Generated Beginner-level Sentence: {beginner_sentence}")
+            else:
+                beginner_sentence = one_to_one_translation(input_text)
+                print(f"Input: {input_text}")
+                print(f"Translation: {beginner_sentence}")
+
+            if make_audio: # synthesize text to speech
                 text_to_speech(beginner_sentence)
-            if save_audio:
-                mp3File = audio_path + japanese_text + ".mp3"
+            if save_audio and make_audio: # Save the speech file to mp3
+                mp3File = audio_path + input_text + ".mp3"
                 save_speech_file(beginner_sentence, mp3File)
             print()
-            save_output_file(japanese_text, beginner_sentence)
+            save_output_file(input_text, beginner_sentence)
 
 ##########
 # Program 
